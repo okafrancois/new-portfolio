5858f60ddc37736d5e4f86d6ac6b37bfb23589e4
bernyfrancois <itoutouberny@gmail.com>
2023-11-29 01:39:30 +0100

    feat: Configure local project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      cooper: ['cooper', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
