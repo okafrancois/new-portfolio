42eda4ddd50da1cfdc0b6c5f1cbea839d48b6ce4
bernyfrancois <itoutouberny@gmail.com>
2024-01-11 13:52:40 +0100

    feat: remove vercel insight

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Header from "./layout/Header.tsx";
 import HomePage from "./pages/home-page/HomePage.tsx";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const customCubic = "cubic-bezier(.36,.85,.18,.97)";
 
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <HomePage />
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
